277d07a5972c108e5e2947fd5b9cacc195119785
Senol <sepp@byteworkshop.de>
2020-01-03 22:34:17 +0100

    Debug

--- app/Template/board/task_image_thumbnail.php
+++ app/Template/board/task_image_thumbnail.php
@@ -8,6 +8,7 @@
 if (! empty($task['owner_id'])): ?>
 <div class="task-board-thumbnails">
     <span>
+		Hello?
         <?= $this->thumbnail->render($task['id'], $task['project_id']) ?>		
     </span>
 </div>
